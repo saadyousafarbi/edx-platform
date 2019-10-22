c4387d48b4b5e1501d80da4ea771884d9ceb09a5
Awais Jibran <awaisdar001@gmail.com>
2019-10-22 14:31:29 +0500

    JavaScript Error when inserting second image in Studio

--- common/test/acceptance/pages/studio/html_component_editor.py
+++ common/test/acceptance/pages/studio/html_component_editor.py
@@ -238,6 +238,32 @@ class HtmlXBlockEditorView(XBlockEditorView):
         """
         click_css(self, '.action-save')
 
+    def open_image_modal(self):
+        """
+        Clicks and in insert image button
+        """
+        # self.q(css='div i[class="mce-ico mce-i-image"]').click()
+        click_css(self, 'div i[class="mce-ico mce-i-image"]')
+
+    def upload_image(self, file_name):
+        """
+        Upload the image via TinyMCE editor.
+        Upload image and add description and click save to upload image via TinyMCE editor.
+        """
+        file_input_css = "[type='file']"
+
+        # select file input element and change visibility to add file.
+        self.browser.execute_script('$("{}").css("display","block");'.format(file_input_css))
+        self.wait_for_element_visibility(file_input_css, "Input is visible")
+        self.q(css=file_input_css).results[0].send_keys(file_name)
+        self.wait_for_element_visibility('#imageDescription', 'Upload form is visible.')
+
+        self.q(css='#imageDescription').results[0].send_keys('test image')
+
+        # click on insert image button
+        click_css(self, '.modal-footer .btn-primary')
+        # self.q(css='.modal-footer .btn-primary').click()
+
 
 class HTMLEditorIframe(XBlockEditorView):
     """
